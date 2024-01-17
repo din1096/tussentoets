6c05d71711ecde617bd0266329e60ea307fab2b3
din1096 <113169715+din1096@users.noreply.github.com>
2024-01-17 10:30:07 +0100

    Add files via upload

---
+++ dungeon.py
@@ -0,0 +1,85 @@
+import random
+import time, math
+player_attack = 1
+player_defense = 0
+player_health = 3
+
+# === [kamer 1] === #
+print('Door de twee grote deuren loop je een gang binnen.')
+print('Het ruikt hier muf en vochtig.')
+print('Je ziet een deur voor je.')
+print('')
+time.sleep(1)
+
+# === [kamer 2] === #
+num1 = random.randint(10,25)
+num2 = random.randint(-5,75)
+print('Je stapt door de deur heen en je ziet een standbeeld voor je.')
+print('Het standbeeld heeft een sleutel vast.')
+print('Op zijn borst zit een numpad met de toesten 9 t/m 0.')
+print(f'Daarboven zie je een som staan {num1} + {num2} =?')
+antwoord = int(input('Wat toest je in?'))
+som = num1 + num2
+if antwoord == som:
+    prijs = 'sleutel'
+    print(f'Het stadbeeld laat de {prijs} vallen en je pakt het op')
+else:
+    print('Er gebeurt niets....')
+
+print('Je zie een deur achter het standbeeld.')
+print('je loopt naar de deur toe en maakt hem open')
+time.sleep(1)
+
+# === [kamer 3] === #
+item = ['schild','zwaart']
+num3 = random.randint(1,10)
+if num3 >= 5:
+    item.remove('zwaart')
+    player_defense += 1
+    print('Je duwt hem open en stap een hele lange kamer binnen.')
+    print(f'In deze kamer staat een tafel met daarop een {item}.')
+    print(f'Je pakt het {item} op en houd het bij je.')
+    print('Op naar de volgende deur.')
+    print('')
+    time.sleep(1)
+else:
+    item.remove('schild')
+    player_attack += 2
+    print('Je duwt hem open en stap een hele lange kamer binnen.')
+    print(f'In deze kamer staat een tafel met daarop een {item}.')
+    print(f'Je pakt het {item} op en houd het bij je.')
+    print('Op naar de volgende deur.')
+    print('')
+    time.sleep(1)
+
+# === [kamer 4] === #
+zombie_attack = 1
+zombie_defense = 0
+zombie_health = 2
+print(f'Dapper met je nieuwe {item} loop je de kamer binnen.')
+print('Je loopt tegen een zombie aan.')
+
+zombie_hit_damage = (zombie_attack - player_defense)
+if zombie_hit_damage <= 0:
+    print('Jij hebt een te goede verdedigign voor de zombie, hij kan je geen schade doen.')
+else:
+    zombie_attack_amount = math.ceil(player_health / zombie_hit_damage)
+    
+    player_hit_damage = (player_attack - zombie_defense)
+    player_attack_amount = math.ceil(zombie_health / player_hit_damage)
+
+    if player_attack_amount < zombie_attack_amount:
+        print(f'In {player_attack_amount} rondes versla je de zombie.')
+        new_playerhealth = int(player_attack_amount * zombie_attack)
+        print(f'Je health is nu {new_playerhealth}.')
+    else:
+        print('Helaas is de zombie te sterk voor je.')
+        print('Game over.')
+        exit()
+print('')
+time.sleep(1)
+
+# === [kamer 5] === #
+print('Voorzichtig open je de deur, je wilt niet nog een zombie tegenkomen.')
+print('Tot je verbazig zie je een schatkist in het midden van de kamer staan.')
+print('Je loopt er naartoe.')
