6ba4433f86f9678f641015a98e7d5f63c6a246ab
dinho <99069865@mydavinci.nl>
2024-01-17 14:33:57 +0100

    'update'

--- test.py
+++ test.py
@@ -17,8 +17,27 @@ kamers = {
         "description":"Je duwt hem open en stap een hele lange kamer binnen.",
      'exits':{'boven':'kamer4'}
     },
+    'kamer4':{
+        'description':"je loopt de kamer binnen en je ziet een spin hij rent naar je toe",
+        'exits':{"links":"kamer5"}
+    },
+    "kamer5":{
+        'description':"Voorzichtig open je de deur, je wilt niet nog een monster tegenkomen.Tot je verbazig zie je een schatkist in het midden van de kamer staan",
+    },
+    "kamer6":{
+        'description':"je loopt een kamer binnen Je ziet een zombie aan hij rent naar je toe",
+        'exits':{'rechts':'kamer3'}
+    },
 }
  
 current_kamer = 'startkamer'
-
-print(kamers[current_kamer]['description'])
+while True:
+    print(kamers[current_kamer]['description'])
+    richting = input('welke kant wil je op?')
+    if richting in ['links','rechts','boven','beneden']:
+        if richting in kamers[current_kamer]['exits']:
+            current_kamer = kamers[current_kamer]['exits'][richting]
+        else:
+            print('je kan de kant niet op')
+    else:
+        print('dat ken ik niet')
