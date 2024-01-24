f575e64cab31d13671201d27c1621c0f84fb0667
dinho <99069865@mydavinci.nl>
2024-01-24 12:52:12 +0100

    'update'

--- dungeon.py
+++ dungeon.py
@@ -1,11 +1,56 @@
 import random
 import time, math
 sleutel = False
-diamanten = 0
+rubies = 0
 player_attack = 1
 player_defense = 0
 player_health = 3
 
+def toverkamer():
+  global player_health
+  global player_defense
+  print('je komt in een kamer en hoort een stem')
+  print('hij zegt dat je mag kiesen tussen meer health of meer defence')
+  print('wat wil je opties: defence health')
+  input('')
+  if input == 'defence':
+    player_defense += 1
+    wapenkamer()
+  elif input == 'health':
+    player_health += 2
+    wapenkamer()
+  
+  
+  
+  
+
+def gokkamer():
+  global rubies
+  global player_health
+  num1 = random.randint(1,6)
+  num2 = random.randint(1,6)
+  som = num1 + num2
+  print('je loopt een kamer in en ziet een grote gokmachine')
+  print('je kan hem gebruiken om je rubies te verdubelen')
+  print('wil je hem gebruiken opties: ja/nee')
+  antwoord = input('')
+  if antwoord == 'ja':
+    if som > 7:
+      rubies * 2
+      print(f'je rubies zijn verdubbelt je hebt {rubies} rubies')
+      toverkamer()
+    elif som < 7:
+      print('je verloor je verliest 1 health')
+      player_health - 1
+      toverkamer()
+    elif som == 7:
+      print('je rubies zijn verduppelt en je verliest 1 health')
+      rubies * 2
+      player_health - 1
+      toverkamer()
+  elif antwoord == 'nee':
+    toverkamer()
+  
 def schatenkamer():
   print('Voorzichtig open je de deur, je wilt niet nog een monster tegenkomen.')
   print('Tot je verbazig zie je een schatkist in het midden van de kamer staan.')
@@ -47,24 +92,24 @@ def spinenkamer():
 def wapenkamer():
   global player_attack
   global player_defense
-  global diamanten
+  global rubies
   print('je komt een nieuwe kamer binnen en ziet een goblin')
-  print('hij zegt dat hij je iets kan verkopen als je een diamant hebt')
-  print(f'je hebt {diamanten} diamanten')
-  if diamanten == 1:
+  print('hij zegt dat hij je iets kan verkopen als je een rubies hebt')
+  print(f'je hebt {rubies} rubies')
+  if rubies == 1:
     print('hij zegt dat je een schild of een zwaard kan kopen')
     userInput = ""
     print('opties: zwaard/schild')
     userInput = input()
     if userInput == 'zwaard':
      print('je koos het zwaard')
-     diamanten - 1
+     rubies - 1
      player_attack += 2
     elif userInput  == 'schild':
      print('je koos het schild')
-     diamanten - 1
+     rubies - 1
      player_defense += 1
-  elif diamanten >= 2:
+  elif rubies >= 2:
     print('je kan een schild en het zwaard kopen')
     print('wil je ze allebij kopen')
     userInput = ''
@@ -72,12 +117,10 @@ def wapenkamer():
     userInput = input()
     if userInput == 'ja':
       print('je hebt het schild en het zwaard')
-      diamanten - 2
+      rubies - 2
       player_defense += 1
       player_attack += 2
   spinenkamer()
-
-
 
 
 def zombiekamer():
@@ -105,27 +148,24 @@ def zombiekamer():
         print('Helaas is de zombie te sterk voor je.')
         print('Game over.')
         exit()
-    wapenkamer()
+    gokkamer()
     time.sleep(1)
 
 def puzzelkamer():
   num1 = random.randint(10,25)
   num2 = random.randint(-5,75)
-  global sleutel
+  global rubies
   print('Je stapt door de deur heen en je ziet een standbeeld voor je.')
-  print('Het standbeeld heeft een sleutel vast.')
+  print('Het standbeeld heeft een ruby vast.')
   print('Op zijn borst zit een numpad met de toesten 9 t/m 0.')
   print(f'Daarboven zie je een som staan {num1} + {num2} =?')
   antwoord = int(input('Wat toest je in?'))
-  global sleutel
   som = num1 + num2
   if antwoord == som:
-    sleutel = True
-    print(f'Het stadbeeld laat de sleutel vallen en je pakt het op')
+    print(f'Het stadbeeld laat de ruby vallen en je pakt het op')
+    rubies += 1
   elif antwoord != som:
-    sleutel = False
-    print('Er gebeurt niets....')
-    
+    print('Er gebeurt niets....')  
   print('je ziet twee verschillende deuren')
   print('welke kant wil je naartoe')
   kanten = ['links','boven']
@@ -136,25 +176,41 @@ def puzzelkamer():
      if userInput == 'links':
        zombiekamer()
      elif userInput == 'boven':
-       wapenkamer()
+       gokkamer()
 
 def diamantenkamer():
-  global diamanten
-  diamanten += 1
+  num1 = random.randint(1,10)
+  global rubies
   print('je komt een kamer binnen met verschillende schaten')
-  print('je ziet in het midden van de kamer een grote diamant je pakt het op')
-  print('je ziet twee verschillende deuren')
-  kanten = ['links','boven']
-  userInput = ""
-  while userInput not in kanten:
-    print("Options: links/boven")
-    userInput = input()
-    if userInput == 'boven':
-     wapenkamer()
-    elif userInput == 'links':
-     puzzelkamer()
-    else:
-      print('print en valid option')
+  if num1 < 10:
+    rubies += 1
+    print('je ziet in het midden van de kamer een grote ruby je pakt het op')
+    print('je ziet twee verschillende deuren')
+    kanten = ['links','boven']
+    userInput = ""
+    while userInput not in kanten:
+     print("Options: links/boven")
+     userInput = input()
+     if userInput == 'boven':
+      gokkamer()
+     elif userInput == 'links':
+      puzzelkamer()
+     else:
+       print('print en valid option')
+  else:
+    print('er zit niks in de schatkisten')
+    print('je ziet twee verschillende deuren')
+    kanten = ['links','boven']
+    userInput = ""
+    while userInput not in kanten:
+     print("Options: links/boven")
+     userInput = input()
+     if userInput == 'boven':
+      gokkamer()
+     elif userInput == 'links':
+      puzzelkamer()
+     else:
+       print('print en valid option')
 
 def introscene():
   print('Door de twee grote deuren loop je een gang binnen.')
